e16c455ec88564d06399080bea275863f92d368b
Axel Padilla <adpadillar25@gmail.com>
2023-09-10 12:49:22 -0600

    progress?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
+import { NextUIProvider } from "@nextui-org/react";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
